f2a179f8dff32782fd2d1ffc015ac8bd10726297
Joseph Toulier Pighi <jtoulier@gmail.com>
2021-02-06 13:23:04 -0500

    A punto de iniciar

---
+++ src/main/java/com/reto2/dataimprovementv1/DataImprovementV1Application.java
@@ -0,0 +1,13 @@
+package com.reto2.dataimprovementv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataImprovementV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataImprovementV1Application.class, args);
+	}
+
+}
